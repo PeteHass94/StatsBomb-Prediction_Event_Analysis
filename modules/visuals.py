c0d85aa807029ee10b9743558e589500be6245f6
PeteHass94 <phassard@hotmail.co.uk>
2025-05-19 23:36:47 +0100

    Update visuals.py
    with dynamic naming

--- modules/visuals.py
+++ modules/visuals.py
@@ -23,12 +23,12 @@ def goal_lines(goals_df, bin_width=10, color='blue', name='Goal'):
         for time in goals_df['timeValue']
     ]
     
-def plot_xg_histograms(binned_for, binned_against, goals_scored, goals_conceded, bin_width=10):
-    x = range(len(binned_for)) * bin_width
+def plot_xg_histograms(binned_for, binned_against, goals_scored, goals_conceded, bin_width=10, for_name="For", against_name="Against"):
+    x = [i * bin_width for i in range(len(binned_for))]
     fig = go.Figure()
 
-    fig.add_trace(go.Bar(x=x, y=binned_for, name=binned_for.name, marker_color='green'))
-    fig.add_trace(go.Bar(x=x, y=binned_against, name=binned_against.name, marker_color='red'))
+    fig.add_trace(go.Bar(x=x, y=binned_for, name=for_name, marker_color='green'))
+    fig.add_trace(go.Bar(x=x, y=binned_against, name=against_name, marker_color='red'))
 
     # Add dashed lines for goals
     for line in goal_lines(goals_scored, bin_width=bin_width, color='goldenrod', name='Goal Scored'):
@@ -45,20 +45,21 @@ def plot_xg_histograms(binned_for, binned_against, goals_scored, goals_conceded,
                              name='Goal Conceded'))
     
     fig.update_layout(
-        title=f"{binned_for.name} and {binned_against.name} Histogram",
+        title=f"{for_name} and {against_name} Histogram",
         xaxis_title="Match Time (mins)",
-        yaxis_title="value",
+        yaxis_title="Value",
         barmode='group',
         showlegend=True
     )
     st.plotly_chart(fig, use_container_width=True)
     
-def plot_rolling_xg(rolling_for, rolling_against, goals_scored, goals_conceded, bin_width=10):
-    x = range(len(rolling_for))  * bin_width
+    
+def plot_rolling_xg(rolling_for, rolling_against, goals_scored, goals_conceded, bin_width=10, for_name="For", against_name="Against"):
+    x = [i * bin_width for i in range(len(rolling_for))]
     fig = go.Figure()
 
-    fig.add_trace(go.Scatter(x=x, y=rolling_for, name=rolling_for.name, line=dict(color='green')))
-    fig.add_trace(go.Scatter(x=x, y=rolling_against, name=rolling_against.name, line=dict(color='red')))
+    fig.add_trace(go.Scatter(x=x, y=rolling_for, name=for_name, line=dict(color='green')))
+    fig.add_trace(go.Scatter(x=x, y=rolling_against, name=against_name, line=dict(color='red')))
 
     # Add vertical lines for goals
     for line in goal_lines(goals_scored, bin_width=bin_width, color='goldenrod', name='Goal Scored'):
@@ -75,7 +76,7 @@ def plot_rolling_xg(rolling_for, rolling_against, goals_scored, goals_conceded, 
                              name='Goal Conceded'))
     
     fig.update_layout(
-        title=f"{rolling_for.name} and {rolling_against.name}",
+        title=f"{for_name} and {against_name} Rolling Line Chart",
         xaxis_title="Match Time (mins)",
         yaxis_title="Rolling value",
         showlegend=True
